3e93109bf22177a680abbb0610324fe8509c07a0
spctravis <spctravis@gmail.com>
2022-12-29 10:00:13 -0700

    Update 22Day5P1.ps1

--- 22Day5P1.ps1
+++ 22Day5P1.ps1
@@ -1,20 +1,28 @@
 #not WORKING AT ALL 
 $counter = 0
-$Xarray = 1..3
-$Yarray = 1..6
-[System.Collections.ArrayList]$array = @()
-$array = $Yarray | foreach {$Y = $_; $Xarray| ForEach { "X" + [string]$_, "Y" + $Y}}
+Remove-Variable -Name column* -ErrorAction SilentlyContinue
+1..3 | foreach {New-Variable -name column$_ -value (New-Object -TypeName 'System.Collections.ArrayList') } 
+
 $inputtext = Get-Content C:\Users\travis.anderson\Documents\AdventOfCode\Day5P1TestInput.txt
 $Lenght = $inputtext.Length
 $line = 1
 $inputtext = 1..$Lenght | ForEach-Object {$inputtext[-$line]; $line++}
 $inputtext = $inputtext.replace("    ", " ") 
-$inputtext.split(" ") | foreach { $array[$counter] = ($array[$counter], $_.trim("[]")); $counter++}
+$inputtext | foreach {
+    $counter++; $_.split(" ") | foreach { $var = $_;$var;
+        switch ($counter) {
+        1 {$column1.add($var.trim("[]"))}
+        2 {$column2.add($var.trim("[]"))}        
+        3 {$column3.add($var.trim("[]"))}
+        }}}
 
 foreach($line in Get-Content C:\Users\travis.anderson\Documents\AdventOfCode\Day5P1Test.txt) {
     $movenum = $line.Split(" ")[1]
     $from = $line.Split(" ")[3]
     $to = $line.Split(" ")[5]
+    switch ($from) {
+    1 { $column1.Insert(
+
     
 } # End Foreach
 $counter
