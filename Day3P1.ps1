093ac0a55e62ba5544ce473358bb314e3fa458bb
spctravis <spctravis@gmail.com>
2023-12-04 18:47:03 -0700

    yu

---
+++ Day3P1.ps1
@@ -0,0 +1,9 @@
+$data = Get-Content "C:\Users\Anderson\Documents\Travis\Advent of code\2023\Day3.txt"
+$linecouter = 0
+$lineend = ($data | Measure-Object).Count
+$symbols = "+", "/","-","*","$","%","#"
+(0..($test.Count-1)) | where {$test[$_] -match 
+$data | foreach {
+    
+    $linecounter++
+     } # end data foreach
